3316becbe3af6deba1230df475b14c0835fa8230
Артём Лашин <sntchweb@yandex.ru>
2023-02-20 17:44:20 +0400

    PEP8 fix

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -36,7 +36,7 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     if (request.user.is_authenticated) and (request.user != author) and not (
-        Follow.objects.filter(user=request.user, author=author).exists()):
+            Follow.objects.filter(user=request.user, author=author).exists()):
         return render(
             request,
             'posts/profile.html',
